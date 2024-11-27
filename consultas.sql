bdb589e3aa24f798e1cfebd5ab68e28ac1d6b63a
manubensio <pereirocondemanuel@gmail.com>
2024-11-27 19:44:18 +0100

    consultas basicas

---
+++ consultas.sql
@@ -0,0 +1,23 @@
+-- seleccionar todos los socios
+SELECT * FROM socio;
+
+-- Obtener todos los libros en préstamo
+SELECT * FROM material_prestamo WHERE tipo = 'libro' AND socio_prestamo IS NOT NULL;
+
+-- seleccionar todos los turnos de un empleado específico
+SELECT * FROM turno WHERE empleado = '00000000Y';
+
+-- Obtener todos los préstamos previos de un socio específico
+SELECT * FROM prestamo_previo WHERE socio_prestamo = '12345678A';
+
+-- seleccionar todos los usos previos de un ordenador específico
+SELECT * FROM uso_previo WHERE id_ordenador = '987654321';
+
+-- seleccionar todos los creadores de nacionalidad estadounidense
+SELECT * FROM creador WHERE nacionalidad = 'EEUU';
+
+-- Obtener todos los materiales prestados por un empleado específico
+SELECT * FROM material_prestamo WHERE empleado_prestamo = '99999999X';
+
+-- Obtener todos los ordenadores prestados a un socio específico
+SELECT * FROM ordenador WHERE usuario = '12345678A';
--- tablas.sql
+++ tablas.sql
@@ -1,14 +1,14 @@
-create table socio(
+CREATE TABLE socio (
     idsocio VARCHAR(9) PRIMARY KEY,
     nombre VARCHAR(20) NOT NULL,
     apellido1 VARCHAR(20) NOT NULL,
     apellido2 VARCHAR(20) NOT NULL,
-    fecha_nacimiento DATE NOT NULL, -- cambiado do diccionario: estes atributos non poden ser nulos
+    fecha_nacimiento DATE NOT NULL,
     direccion VARCHAR(30) NOT NULL,
     telefono VARCHAR(12) NOT NULL
 );
 
-CREATE TABLE empleado(
+CREATE TABLE empleado (
     dni CHAR(9) PRIMARY KEY,
     nombre VARCHAR(20) NOT NULL,
     apellido1 VARCHAR(20) NOT NULL,
@@ -18,52 +18,26 @@ CREATE TABLE empleado(
     salario FLOAT NOT NULL
 );
 
-CREATE TABLE cargo(
+CREATE TABLE cargo (
     dni_empleado CHAR(9) PRIMARY KEY,
     cargo VARCHAR(20) NOT NULL,
-    FOREIGN KEY (DNI_empleado) REFERENCES empleado(DNI)
+    FOREIGN KEY (dni_empleado) REFERENCES empleado(dni)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
-CREATE TABLE turno(
+CREATE TABLE turno (
     empleado CHAR(9),
     dia DATE NOT NULL,
-    PRIMARY KEY (empleado, dia), -- modificado para que un empleado pueda tener múltiples turnos
+    PRIMARY KEY (empleado, dia), -- Un empleado puede tener múltiples turnos
     hora_entrada TIME NOT NULL,
     hora_salida TIME NOT NULL,
-    FOREIGN KEY (empleado) REFERENCES empleado(DNI)
+    FOREIGN KEY (empleado) REFERENCES empleado(dni)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
-CREATE TABLE material_prestamo(
-    id VARCHAR(9) PRIMARY KEY,
-    tipo VARCHAR(10) NOT NULL, --libro o cd
-    titulo VARCHAR(20) NOT NULL,
-    fecha_publicacion DATE NOT NULL,
-    creador VARCHAR(9) NOT NULL,
-    productora VARCHAR(20) NOT NULL,
-    isbn VARCHAR(13) NOT NULL,
-
-    socio_prestamo VARCHAR(9),
-    empleado_prestamo CHAR(9), --segun el modelo relacional pueden ser nulos 
-    fecha_prestamo DATE, 
-    disponible BOOLEAN GENERATED ALWAYS AS (socio_prestamo IS NULL) STORED, -- nuevo atributo: 'disponible' si el material está disponible o no
-    
-    FOREIGN KEY (socio_prestamo) REFERENCES socio(idsocio)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE,
-    FOREIGN KEY (empleado_prestamo) REFERENCES empleado(DNI)
-    ON DELETE SET NULL
-    ON UPDATE CASCADE,
-    FOREIGN KEY (creador) REFERENCES creador(idcreador)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE
-    CHECK (tipo IN ('libro', 'CD'))
-); 
-
-CREATE TABLE creador(
+CREATE TABLE creador (
     idcreador VARCHAR(9) PRIMARY KEY,
     nombre VARCHAR(20) NOT NULL,
     apellido1 VARCHAR(20),
@@ -72,16 +46,42 @@ CREATE TABLE creador(
     nacionalidad VARCHAR(20)
 );
 
-CREATE TABLE pertenecer_estilo(
-    material VARCHAR(9),
-    estilo VARCHAR(20) NOT NULL,
-    FOREIGN KEY (material) REFERENCES material_prestamo(material_prestamo)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
-    PRIMARY KEY (id_creador, estilo)
+CREATE TABLE material_prestamo (
+    id VARCHAR(9) PRIMARY KEY,
+    tipo VARCHAR(10) NOT NULL, -- 'libro' o 'cd'
+    titulo VARCHAR(20) NOT NULL,
+    genero VARCHAR(20) NOT NULL,
+    fecha_publicacion DATE NOT NULL,
+    creador VARCHAR(9) NOT NULL,
+    productora VARCHAR(20) NOT NULL,
+    isbn VARCHAR(13), -- null en cds
+
+    socio_prestamo VARCHAR(9),
+    empleado_prestamo CHAR(9), -- Puede ser NULL
+    fecha_prestamo DATE, 
+    
+    FOREIGN KEY (socio_prestamo) REFERENCES socio(idsocio)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE,
+    FOREIGN KEY (empleado_prestamo) REFERENCES empleado(dni)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE,
+    FOREIGN KEY (creador) REFERENCES creador(idcreador)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE,
+    CHECK (tipo IN ('libro', 'CD')),
 );
 
-CREATE TABLE prestamo_previo(
+CREATE TABLE pertenecer_estilo (
+    creador VARCHAR(9),
+    estilo VARCHAR(20) NOT NULL,
+    FOREIGN KEY (creador) REFERENCES creador(idcreador)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    PRIMARY KEY (creador, estilo)
+);
+
+CREATE TABLE prestamo_previo (
     fecha_prestamo DATE,
     fecha_devolucion DATE,
     socio_prestamo VARCHAR(9),
@@ -89,45 +89,45 @@ CREATE TABLE prestamo_previo(
     material VARCHAR(9),
     FOREIGN KEY (socio_prestamo) REFERENCES socio(idsocio)
     ON DELETE SET NULL
-    ON UPDATE CASCADE
-    FOREIGN KEY (empleado_prestamo) REFERENCES empleado(DNI)
+    ON UPDATE CASCADE,
+    FOREIGN KEY (empleado_prestamo) REFERENCES empleado(dni)
     ON DELETE SET NULL
-    ON UPDATE CASCADE
+    ON UPDATE CASCADE,
     FOREIGN KEY (material) REFERENCES material_prestamo(id)
     ON DELETE CASCADE
-    ON UPDATE CASCADE
+    ON UPDATE CASCADE,
     PRIMARY KEY (fecha_prestamo, socio_prestamo, empleado_prestamo, material)
 );
 
-CREATE TABLE ordenador(
-    ID_ordenador VARCHAR(9) PRIMARY KEY,
-    SO VARCHAR(10),
+CREATE TABLE ordenador (
+    id_ordenador VARCHAR(9) PRIMARY KEY,
+    so VARCHAR(10),
     modelo VARCHAR(20),
-    usuario  VARCHAR(9),
+    usuario VARCHAR(9),
     fecha_prestamo DATE,
-    empleado_prestamo CHAR(9), 
+    empleado_prestamo CHAR(9),
     FOREIGN KEY (usuario) REFERENCES socio(idsocio)
     ON DELETE NO ACTION
-    ON UPDATE CASCADE
-    FOREIGN KEY (empleado_prestamo) REFERENCES empleado(DNI)
+    ON UPDATE CASCADE,
+    FOREIGN KEY (empleado_prestamo) REFERENCES empleado(dni)
     ON DELETE SET NULL
     ON UPDATE CASCADE
 );
 
-CREATE TABLE uso_previo(
+CREATE TABLE uso_previo (
     fecha_prestamo DATE,
-    ID_ordenador VARCHAR(9),
-    usuario  VARCHAR(9),
+    id_ordenador VARCHAR(9),
+    usuario VARCHAR(9),
     fecha_devolucion DATE,
     empleado_prestamo CHAR(9),
     FOREIGN KEY (usuario) REFERENCES socio(idsocio)
     ON DELETE SET NULL
-    ON UPDATE CASCADE
-    FOREIGN KEY (empleado_prestamo) REFERENCES empleado(DNI)
+    ON UPDATE CASCADE,
+    FOREIGN KEY (empleado_prestamo) REFERENCES empleado(dni)
     ON DELETE SET NULL
-    ON UPDATE CASCADE
-    FOREIGN KEY (ID_ordenador) REFERENCES ordenador(ID_ordenador)
+    ON UPDATE CASCADE,
+    FOREIGN KEY (id_ordenador) REFERENCES ordenador(id_ordenador)
     ON DELETE CASCADE
-    ON UPDATE CASCADE
-    PRIMARY KEY (fecha_prestamo, usuario, empleado_prestamo, ID_ordenador)
+    ON UPDATE CASCADE,
+    PRIMARY KEY (fecha_prestamo, usuario, empleado_prestamo, id_ordenador)
 );
