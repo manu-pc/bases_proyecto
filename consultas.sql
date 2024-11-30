604c7179320d28f77cbf8b82a03b65e4446d4304
manubensio <pereirocondemanuel@gmail.com>
2024-11-30 15:37:01 +0100

    arregladas consultas para funcionar

--- consultas.sql
+++ consultas.sql
@@ -4,11 +4,8 @@ SELECT * FROM socio;
 -- seleccionar todos los creadores
 SELECT * FROM creador;
 
--- seleccionar todo el material
-SELECT * FROM material_prestamo;
-
--- ver todos los prestamos previos
-SELECT * FROM prestamo_previo;
+-- seleccionar los nombres, tipos e id de todos los materiales
+SELECT titulo, tipo, id_material FROM material_prestamo;
 
 -- seleccionar todos los ordenadores
 SELECT * FROM ordenador;
@@ -16,21 +13,15 @@ SELECT * FROM ordenador;
 -- Obtener todos los libros en préstamo
 SELECT * FROM material_prestamo WHERE tipo = 'libro' AND socio_prestamo IS NOT NULL;
 
--- seleccionar todos los turnos de un empleado específico
+-- seleccionar todos los turnos de un empleado específico (Juan)
 SELECT * FROM turno WHERE empleado = '00000000Y';
 
 -- Obtener todos los préstamos previos de un socio específico
-SELECT * FROM prestamo_previo WHERE socio_prestamo = '12345678A';
+SELECT * FROM prestamo_previo WHERE socio_prestamo = 1;
 
 -- seleccionar todos los usos previos de un ordenador específico
-SELECT * FROM uso_previo WHERE id_ordenador = '987654321';
+SELECT * FROM uso_previo WHERE id_ordenador = 3000000;
 
--- seleccionar todos los creadores de nacionalidad estadounidense
-SELECT * FROM creador WHERE nacionalidad = 'EEUU';
+-- seleccionar todos los nombres de los creadores de nacionalidad estadounidense
+SELECT nombre, apellido1, apellido2 FROM creador WHERE nacionalidad = 'EEUU';
 
-
--- Obtener todos los materiales prestados por un empleado específico
-SELECT * FROM material_prestamo WHERE empleado_prestamo = '99999999X';
-
--- Obtener todos los ordenadores prestados a un socio específico
-SELECT * FROM ordenador WHERE usuario = '12345678A';
